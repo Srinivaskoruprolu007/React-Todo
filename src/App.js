b123345e14f6d411793b355ab4b03ef2837437e4
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2023-12-15 18:17:49 +0530

    some modification are done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [todos, setTodos] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let todo = document.getElementById("todo-Add").value;
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo.trim(),
+      completed: false,
+    };
+    if (newTodo.text.length > 0) {
+      setTodos([...todos].concat(newTodo));
+    } else {
+      alert("Enter a valid task");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <div id="todo-list" className="w-500px m-60px">
+        <h1 className="text-center font-serif text-blue-600 text-3xl bg-gray-600 ">
+          Todo List
+        </h1>
+        <form onSubmit={handleSubmit} id="todo-Add">
+          <div className="flex flex-col items-center mt-6 ">
+          <input type="text" class="border border-gray-400 rounded p-2 w-[400px] transition-all duration-300 hover:border-blue-500 focus:border-blue-500 focus:outline-none" id='todo-Add'/>
+
+            <button type="submit" className="bg-blue-900 text-white border  rounded-md mt-5 p-2 hover:bg-blue-200 ">Add Todo</button>
+          </div>
+        </form>
+        {todos.map((todo) => {
+
+        })}
+      </div>
     </div>
   );
 }
-
-export default App;
